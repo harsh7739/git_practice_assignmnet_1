4e63fa33f6af1932fe454ede6a88d91a53e1204a
harsh7739 <hrbhumi123@gmail.com>
2022-12-04 19:38:19 +0530

    prime and palindrome problem completed

---
+++ Palindrome_problem.js
@@ -0,0 +1,12 @@
+let str = naman;
+let str1= "";
+for(let i=0;i<str.length;i++){
+    str1 += str[i]
+    }
+}
+   if(str==str1){
+       console.log("it is palindrome")
+    }
+      else{
+         console.log("not aplindrome");
+      }  
---
+++ Prime_problem.js
@@ -0,0 +1,10 @@
+let num = 17;
+let count = 0;
+for(let i=0;i<num;i++){
+   if(num%i==0){
+    count++;
+   }
+}
+if(count==2){
+console.log(num,"is prime")
+}
