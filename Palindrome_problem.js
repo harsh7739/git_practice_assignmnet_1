da4900db21fa6f0438e9da79675ce32f638cba48
Kuldeep Kumar Baitha <kuldeepkumar83986@gmail.com>
2022-12-04 22:57:00 +0530

    code is fixed now

--- Palindrome_problem.js
+++ Palindrome_problem.js
@@ -1,12 +1,12 @@
-let str = naman;
-let str1= "";
-for(let i=0;i<str.length;i++){
-    str1 += str[i]
-    }
+let str = "naman";
+let str1 = "";
+for (let i = str.length-1; i>=0; i--) {
+   str1 += str[i]
 }
-   if(str==str1){
-       console.log("it is palindrome")
-    }
-      else{
-         console.log("not aplindrome");
-      }  
+
+if (str == str1) {
+   console.log("it is palindrome")
+}
+else {
+   console.log("not aplindrome");
+}  
